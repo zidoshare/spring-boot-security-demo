b4de0c1d13d0f3a3e9111fc1a31e6c7b464164dc
zido <wuhongxu1208@gmail.com>
2019-08-04 15:44:47 +0800

    initial project

---
+++ README.md
@@ -0,0 +1,11 @@
+# 通过官方demo简析spring security原理
+
+# 项目构建
+
+戳>>>>>>>[官方文档](https://spring.io/guides/gs/securing-web/)
+
+戳>>>>>>>[我的示例](https://github.com/zidoshare/spring-boot-security-demo)
+
+# 从spring boot的逻辑里看spring security如何生效
+
+
---
+++ src/main/java/site/zido/demo/common/MvcConfig.java
@@ -0,0 +1,20 @@
+package site.zido.demo.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author zido
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
